d3e847d0995548ac2cd5716bfaf146e82e59c5a4
Desire Chukwudi <chukwudidesire01@gmail.com>
2023-05-04 07:03:39 +0100

    First Commit

---
+++ script.js
@@ -0,0 +1,8 @@
+let labels = document.querySelectorAll(".form-control label");
+
+labels.forEach((label) => {
+  label.innerHTML = label.innerText
+    .split("")
+    .map((letter, indx) => `<span style="transition-delay: ${indx * 80}ms">${letter}</span>`)
+    .join("");
+});
